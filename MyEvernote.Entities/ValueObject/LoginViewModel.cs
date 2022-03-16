486cdc7b77d4a28990763e02f66c56646a6f6261
Rumeysa Akcanca <94036242+rumeysa-akcanca@users.noreply.github.com>
2022-03-16 10:31:13 +0300

    Update LoginViewModel.cs
    .

--- MyEvernote.Entities/ValueObject/LoginViewModel.cs
+++ MyEvernote.Entities/ValueObject/LoginViewModel.cs
@@ -10,10 +10,10 @@ namespace MyEvernote.Entities.ValueObject
     public class LoginViewModel
     {
         [DisplayName("Username"), Required(ErrorMessage = "{0} field cannot be left blank"), StringLength(25,
-          ErrorMessage = "{0} max. {1} karakter olmalı.")]
+          ErrorMessage = "{0}  should be max. {1} characters.")]
         public string Username { get; set; }
         [DisplayName("Password"), Required(ErrorMessage = "{0} field cannot be left blank"),DataType(DataType.Password), StringLength(25,
-          ErrorMessage = "{0} max. {1} karakter olmalı.")] // alanı boş geçilemez
+          ErrorMessage = "{0}  should be max. {1} characters.")] // alanı boş geçilemez
         public string Password { get; set; }
     }
 }
